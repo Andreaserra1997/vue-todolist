027f7322ef3b87304e02588d6435ebe3bd034652
Andreaserra1997 <serra.andrea1997@gmail.com>
2023-05-09 17:18:27 +0200

    add line-through

--- js/script.js
+++ js/script.js
@@ -3,20 +3,28 @@ const app = Vue.createApp({
         return {
             todos: [
                 {
-                    text:'',
+                    text:'asdfg',
                     done: true,
                 },
                 {
-                    text:'',
+                    text:'safdf',
                     done: true,
                 },
                 {
-                    text:'',
+                    text:'sdfg',
                     done: true,
-                }
+                },
             ]
         };
     },
+    methods: {
+        todoDone(i) {
+            return this.todos[i].done = !this.todos[i].done;
+        },
+        deleteElement(index) {
+            this.todos.splice(index, 1);
+        },
+    }
 });
 
 app.mount('#app')
